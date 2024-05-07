65b9515a99db2e33e8fa5baeff9c4b6a7e8dcbb4
AQA'S LAPTOP <AQA'S LAPTOP@LAPTOP-AA3N1G6J>
2024-05-07 11:55:12 +0700

    14

--- part 5/part 5.cpp
+++ part 5/part 5.cpp
@@ -14,3 +14,21 @@ class pelajar
 private:
     int nilai;
 
+public:
+    pelajar() { nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
+void manusia::showNilaiPelajar(pelajar& x)
+{
+    cout << x.nilai;
+}
+
+
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    budi.showNilaiPelajar(pbudi);
+    return 0;
+}14
